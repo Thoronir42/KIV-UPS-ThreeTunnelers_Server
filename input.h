d77959d67410afb419987c2464b0918fb3204ad6
Thoronir42 <Thoronir42@Gmail.com>
2016-03-05 00:08:35 +0100

    Added input data structure

---
+++ input.h
@@ -0,0 +1,24 @@
+#ifndef INPUT_H
+#define	INPUT_H
+
+#define INPUT_MASK_UP 0x01
+#define INPUT_MASK_DOWN 0x02
+#define INPUT_MASK_LEFT 0x04
+#define INPUT_MASK_RIGHT 0x08
+#define INPUT_MASK_SHOOT 0x10
+
+typedef struct input{
+	int heldKeys;
+} input;
+
+
+void input_key_press(input *p_input, int input_type){
+	p_input->heldKeys |= input_type;
+}
+
+void input_key_press(input *p_input, int input_type){
+	p_input->heldKeys &= ~input_type;
+}
+
+#endif	/* INPUT_H */
+
