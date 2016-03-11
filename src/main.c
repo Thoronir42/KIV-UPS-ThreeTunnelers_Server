bc5248cf2e59dfae937b0315fe630e947b70cc75
Thoronir42 <Thoronir42@Gmail.com>
2016-03-11 22:45:04 +0100

    Introduced direction module

--- src/main.c
+++ src/main.c
@@ -12,6 +12,7 @@
 #include "game/input.h"
 #include "game/tank.h"
 #include "game/tunneler_map_block.h"
+#include "game/direction.h"
 
 #define _CHUNK_SIZE 20
 #define _MAX_TICKRATE 32
@@ -49,6 +50,25 @@ int define_consts(){
 	*(int *)&MAP_BLOCK_ROCK = 3;
 	*(int *)&MAP_BLOCK_WALL = 4;
 	
+	//			DIRECTIONS
+	*(unsigned short *)&DIRECTION_0 = 0;
+	*(unsigned short *)&DIRECTION_N = 1;
+	*(unsigned short *)&DIRECTION_NE = 2;
+	*(unsigned short *)&DIRECTION_E = 3;
+	*(unsigned short *)&DIRECTION_SE = 4;
+	*(unsigned short *)&DIRECTION_S = 5;
+	*(unsigned short *)&DIRECTION_SW = 6;
+	*(unsigned short *)&DIRECTION_W = 7;
+	*(unsigned short *)&DIRECTION_NW = 8;
+	
+	DIRECTION_AXIS_X[DIRECTION_NW] = DIRECTION_AXIS_X[DIRECTION_W] = DIRECTION_AXIS_X[DIRECTION_SW] =-1;
+	DIRECTION_AXIS_X[DIRECTION_N]  = DIRECTION_AXIS_X[DIRECTION_0] = DIRECTION_AXIS_X[DIRECTION_S]  = 0;
+	DIRECTION_AXIS_X[DIRECTION_NE] = DIRECTION_AXIS_X[DIRECTION_E] = DIRECTION_AXIS_X[DIRECTION_SE] = 1;
+	
+	DIRECTION_AXIS_Y[DIRECTION_NW] = DIRECTION_AXIS_Y[DIRECTION_N] = DIRECTION_AXIS_Y[DIRECTION_NE] =-1;
+	DIRECTION_AXIS_Y[DIRECTION_W]  = DIRECTION_AXIS_Y[DIRECTION_0] = DIRECTION_AXIS_Y[DIRECTION_E]  = 0;
+	DIRECTION_AXIS_Y[DIRECTION_SW] = DIRECTION_AXIS_Y[DIRECTION_S] = DIRECTION_AXIS_Y[DIRECTION_SE] = 1;
+	
 	return 0;
 }
 
@@ -62,13 +82,13 @@ int main(int argc, char* argv[]) {
 	if(settings_process_arguments(p_settings, argc, argv)){
 		print_help();
 	}
-	settings *p_settings = settings_process_arguments(argc - 1, argv + 1);
+	
 	*(int *)(&p_settings->CHUNK_SIZE) = _CHUNK_SIZE;
 	*(unsigned int *)&p_settings->MAX_TICKRATE = _MAX_TICKRATE;
 	
 	networks *p_networks = networks_create(p_settings);
 	if(!p_networks){
-		printf("Network interface couldn't be created, exitting. ");
+		printf("Network interface couldn't be created, exitting. \n");
 		return RUN_ERROR_NETWORK_FAILED;
 	}
 	
